e8dd4b49fdccc05d1f7701daced7ea70692c52b0
Viraj Shinde <virajabhayshinde@gmail.com>
2021-09-18 12:22:02 +0530

    Images are getting displayed

--- client/src/Components/Files.js
+++ client/src/Components/Files.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react'
 const  Files = (props) => {
 
     const images = props.images.map((image) =>{
-        return image.urls.full
+        return <img src={image.urls.full}/>
         
     })
 
